f11a45cd2aa6453d38a7c9880e7ecce171b72ede
zhangyixiao <zhangyixiao@kezaihui.com>
2021-10-29 10:07:07 +0800

    帖子详情支持二级评论

--- src/main/java/com/laosiji/qasystem/domain/ro/LikeRo.java
+++ src/main/java/com/laosiji/qasystem/domain/ro/LikeRo.java
@@ -1,9 +1,14 @@
 package com.laosiji.qasystem.domain.ro;
 
 
+import com.laosiji.qasystem.domain.enums.LikeType;
+import com.laosiji.qasystem.domain.enums.PCCategory;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 /**
  * @author zhangyixiao
@@ -12,5 +17,24 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class LikeRo {
+
+    private Long postId;
+
+    private Long commentId;
+
+    private LikeType likeType;
+
+    private PCCategory likeCategory;
+
+    private Long authorId;
+
+    private String authorName;
+
+    private LocalDateTime deletedAt;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 }
